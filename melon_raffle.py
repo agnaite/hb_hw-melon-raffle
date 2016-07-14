b19a68d43ddf29aa114fb64fd76526f71aa513a0
Agnė Klimaitė <agnaite@me.com>
2016-07-13 23:23:39 -0700

    split up customer_melon_raffle into separate files and fixed import

--- melon_raffle.py
+++ melon_raffle.py
@@ -1,5 +1,23 @@
+from random import choice
+import customer_info
+
 """Randomly pick customer and print customer info"""
 
 # Add code starting here
 # Hint: remember to import any functions you need from
     # other files or libraries
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print "Contact {name} at {email} to notify them they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+customers = customer_info.organize_customer_data("customers.txt")
+
+pick_winner(customers)
